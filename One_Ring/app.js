ad2eed8516a3a08c8eb30aa1f23c7b6758ccf959
Job85 <julianjernigan@ymail.com>
2022-03-08 16:55:06 -0500

    added event listeners

--- One_Ring/app.js
+++ One_Ring/app.js
@@ -1,93 +1,146 @@
-console.log('JS is connected')
 
 
+const letsPlay = document.querySelector('#letsPlay');
 
-questions = [
-    'How long did J.R.R Tolkien spend completing the Lord of the Rings books?',
-    'How many rings were given to the Dwarf-kings?',
-    'What was Frodo\'s Father\'s name?',
-    'What was Sam\'s spouse\'s name?',
-    'How many Nazgul were there?',
-    'Who was the leader of the Nazgul?',
-    'Who were the Istari?',
-    'What was the Dark Lord\'s name?',
-    'How many were the Fathers of the Dwarves?',
-    'Who were the Wood-elves?',
-    'What is the name of Elrond\'s daughter?'
-]
-answers = [
-    ['a. 5 years', 'b. 12 years', 'c. 25 years', 'D. 40 years'],
-    ['a. 9 rings', 'B. 7 rings', 'c. 3 rings', ' d. 1 ring'],
-    ['a. Bilbo', 'B. Drogo', 'c. Fosco', 'd. Largo'],
-    ['A. Rose', 'b. Elanor', 'c. Bree', 'd. Arwen'],
-    ['a. 6', 'b. 12', 'C. 9', 'd. 15'],
-    ['a. Grima', 'b. Dunedain', 'c. Khamul', 'D. Witch-king of Angmar']
-    ['A. Wizards', 'b. Balrog', 'c. Ents', 'd. Dragons'],
-    ['a. Saruman', 'b. Melkor', 'c. Smaug', 'D. Sauron'],
-    ['a. 12', 'B. 7', 'c. 6', 'd. 10'],
-    ['A. Silvan', 'b. Laiquendi', 'c. Vanyar', 'd. Falmari'],
-    ['a. ', 'b. ', 'c. ', 'd. Arwen'],
-    ['a. ', 'b. ', 'c. ', 'd. '],
-    ['a. ', 'b. ', 'c. ', 'd. '],
-    ['a. ', 'b. ', 'c. ', 'd. '],
-    ['a. ', 'b. ', 'c. ', 'd. '],
-    ['a. ', 'b. ', 'c. ', 'd. '],
-    ['a. ', 'b. ', 'c. ', 'd. '],
-    ['a. ', 'b. ', 'c. ', 'd. '],
-    ['a. ', 'b. ', 'c. ', 'd. '],
-    ['a. ', 'b. ', 'c. ', 'd. '],
-    ['a. ', 'b. ', 'c. ', 'd. '],
-    ['a. ', 'b. ', 'c. ', 'd. '],
-    ['a. ', 'b. ', 'c. ', 'd. '],
-    ['a. ', 'b. ', 'c. ', 'd. '],
-    ['a. ', 'b. ', 'c. ', 'd. '],
-    ['a. ', 'b. ', 'c. ', 'd. '],
-    ['a. ', 'b. ', 'c. ', 'd. '],
-    ['a. ', 'b. ', 'c. ', 'd. '],
-    ['a. ', 'b. ', 'c. ', 'd. '],
-    ['a. ', 'b. ', 'c. ', 'd. '],
-    ['a. ', 'b. ', 'c. ', 'd. '],
-    ['a. ', 'b. ', 'c. ', 'd. '],
-    ['a. ', 'b. ', 'c. ', 'd. '],
-    ['a. ', 'b. ', 'c. ', 'd. '],
-    ['a. ', 'b. ', 'c. ', 'd. '],
-    ['a. ', 'b. ', 'c. ', 'd. '],
-    ['a. ', 'b. ', 'c. ', 'd. '],
-    ['a. ', 'b. ', 'c. ', 'd. '],
-    ['a. ', 'b. ', 'c. ', 'd. '],
-    ['a. ', 'b. ', 'c. ', 'd. ']
-],
-    correctAnswers = [],
-    playerAnswers = [];
+const easyLvl = document.querySelector('#easyLvl')
+const mediumLvl = document.querySelector('#mediumLvl')
+const hardLvl = document.querySelector('#hardLvl')
+const expertLvl = document.querySelector('#expertLvl')
 
-const player1 = 'Player 1 has WON!';
-const player2 = 'Player 2 has WON!';
+const btnA = document.querySelector('#btnA')
+const btnB = document.querySelector('#btnB')
+const btnC = document.querySelector('#btnC')
+const btnD = document.querySelector('#btnD')
 
-
-const start = document.querySelector('#start')
 const submit = document.querySelector('#submit')
 const reset = document.querySelector('#reset')
-const letsPlay = document.querySelector('#letsPlay')
+let player1Score = 0;
+let player2Score = 0;
+// answerArray = [btnA, btnB, btnC, btnD];
+// playerQuestions = [];
 
-const buttonA = document.querySelector('#buttonA')
-const buttonB = document.querySelector('#buttonB')
-const buttonC = document.querySelector('#buttonC')
-const buttonD = document.querySelector('#buttonD')
 
-letsPlay.addEventListener('click', onclick)
-function () {
+// eLvlQuestions = [
+//     'How long did J.R.R Tolkien spend completing the Lord of the Rings books?',
+//     'How many rings were given to the Dwarf-kings?',
+//     'What was Frodo\'s Father\'s name?',
+//     'What was Sam\'s spouse\'s name?',
+//     'How many Nazgul were there?',
+//     'Who was the leader of the Nazgul?',
+//     'Who were the Istari?',
+//     'What was the Dark Lord\'s name?',
+//     'How many were the Fathers of the Dwarves?',
+//     'Who were the Wood-elves?',
+//     'What is the name of Elrond\'s daughter?'
+// ];
+// eLvlAnswers = [
+//     ['a. 5 years', 'b. 12 years', 'c. 25 years', 'D. 40 years'],
+//     ['a. 9 rings', 'B. 7 rings', 'c. 3 rings', ' d. 1 ring'],
+//     ['a. Bilbo', 'B. Drogo', 'c. Fosco', 'd. Largo'],
+//     ['A. Rose', 'b. Elanor', 'c. Bree', 'd. Arwen'],
+//     ['a. 6', 'b. 12', 'C. 9', 'd. 15'],
+//     ['a. Grima', 'b. Dunedain', 'c. Khamul', 'D. Witch-king of Angmar']
+//     ['A. Wizards', 'b. Balrog', 'c. Ents', 'd. Dragons'],
+//     ['a. Saruman', 'b. Melkor', 'c. Smaug', 'D. Sauron'],
+//     ['a. 12', 'B. 7', 'c. 6', 'd. 10'],
+//     ['A. Silvan', 'b. Laiquendi', 'c. Vanyar', 'd. Falmari'],
+//     ['a. ', 'b. ', 'c. ', 'd. Arwen'],
+//     ['a. ', 'b. ', 'c. ', 'd. '],
+//     ['a. ', 'b. ', 'c. ', 'd. '],
+//     ['a. ', 'b. ', 'c. ', 'd. '],
+//     ['a. ', 'b. ', 'c. ', 'd. '],
+//     ['a. ', 'b. ', 'c. ', 'd. '],
+//     ['a. ', 'b. ', 'c. ', 'd. '],
+//     ['a. ', 'b. ', 'c. ', 'd. '],
+//     ['a. ', 'b. ', 'c. ', 'd. '],
+//     ['a. ', 'b. ', 'c. ', 'd. '],
+//     ['a. ', 'b. ', 'c. ', 'd. '],
+//     ['a. ', 'b. ', 'c. ', 'd. '],
+//     ['a. ', 'b. ', 'c. ', 'd. '],
+//     ['a. ', 'b. ', 'c. ', 'd. '],
+//     ['a. ', 'b. ', 'c. ', 'd. '],
+//     ['a. ', 'b. ', 'c. ', 'd. '],
+//     ['a. ', 'b. ', 'c. ', 'd. '],
+//     ['a. ', 'b. ', 'c. ', 'd. '],
+//     ['a. ', 'b. ', 'c. ', 'd. '],
+//     ['a. ', 'b. ', 'c. ', 'd. '],
+//     ['a. ', 'b. ', 'c. ', 'd. '],
+//     ['a. ', 'b. ', 'c. ', 'd. '],
+//     ['a. ', 'b. ', 'c. ', 'd. '],
+//     ['a. ', 'b. ', 'c. ', 'd. '],
+//     ['a. ', 'b. ', 'c. ', 'd. '],
+//     ['a. ', 'b. ', 'c. ', 'd. '],
+//     ['a. ', 'b. ', 'c. ', 'd. '],
+//     ['a. ', 'b. ', 'c. ', 'd. '],
+//     ['a. ', 'b. ', 'c. ', 'd. '],
+//     ['a. ', 'b. ', 'c. ', 'd. ']
+// ];
+// correctAnswers = ['d', 'b', 'b', 'a', 'c', 'd', 'a', 'd', 'b', 'a'],
+//     playerAnswers = [];
+
+
+
+
+// letsPlay.addEventListener('click', goToGame);
+// function goToGame() {
+//     console.log('button clicked')
+// }
+btnA.addEventListener('click', selectA)
+btnB.addEventListener('click', selectB)
+btnC.addEventListener('click', selectC)
+btnD.addEventListener('click', selectD)
+
+easyLvl.addEventListener('click', chooseEasy)
+mediumLvl.addEventListener('click', chooseMedium)
+hardLvl.addEventListener('click', chooseHard)
+expertLvl.addEventListener('click', chooseExpert)
+
+function selectA() {
 
 }
 
-start.addEventListener('click', onclick)
-function () {
+function selectB() {
 
 }
-console.log('start button clicked')
 
-
-submit.addEventListener('click', onclick)
-function () {
+function selectC() {
 
 }
-reset.addEventListener('click', onclick)
+function selectD() {
+
+}
+
+
+function chooseEasy() {
+    console.log('button easy clicked')
+}
+
+function chooseMedium() {
+    console.log('button md clicked')
+}
+
+function chooseHard() {
+    console.log('button hard clicked')
+}
+
+function chooseExpert() {
+    console.log('button expert clicked')
+}
+// submit.addEventListener('click', onclick)
+
+// reset.addEventListener('click', onclick)
+
+
+
+
+
+
+
+
+
+//correct answer sequence
+
+//incorrect answer sequence
+
+
+
