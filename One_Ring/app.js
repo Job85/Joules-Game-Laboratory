273f7092b1c69ed94eee22695709171e8473e179
Job85 <julianjernigan@ymail.com>
2022-03-08 11:05:50 -0500

    added about html

--- One_Ring/app.js
+++ One_Ring/app.js
@@ -1,1 +1,78 @@
 console.log('JS is connected')
+
+questions = [
+    'How long did J.R.R Tolkien spend completing the Lord of the Rings books?',
+    'How many rings were given to the Dwarf-kings?',
+    'What was Frodo\'s Father\'s name?',
+    'What was Sam\'s spouse\'s name?',
+    'How many Nazgul were there?',
+    'Who was the leader of the Nazgul?',
+    'Who were the Istari?',
+    'What was the Dark Lord\'s name?',
+    'How many were the Fathers of the Dwarves?',
+    'Who were the Wood-elves?',
+    'What is the name of Elrond\'s daughter?'
+]
+answers = [
+    ['a. 5 years', 'b. 12 years', 'c. 25 years', 'D. 40 years'],
+    ['a. 9 rings', 'B. 7 rings', 'c. 3 rings', ' d. 1 ring'],
+    ['a. Bilbo', 'B. Drogo', 'c. Fosco', 'd. Largo'],
+    ['A. Rose', 'b. Elanor', 'c. Bree', 'd. Arwen'],
+    ['a. 6', 'b. 12', 'C. 9', 'd. 15'],
+    ['a. Grima', 'b. Dunedain', 'c. Khamul', 'D. Witch-king of Angmar']
+    ['A. Wizards', 'b. Balrog', 'c. Ents', 'd. Dragons'],
+    ['a. Saruman', 'b. Melkor', 'c. Smaug', 'D. Sauron'],
+    ['a. 12', 'B. 7', 'c. 6', 'd. 10'],
+    ['A. Silvan', 'b. Laiquendi', 'c. Vanyar', 'd. Falmari'],
+    ['a. ', 'b. ', 'c. ', 'd. Arwen'],
+    ['a. ', 'b. ', 'c. ', 'd. '],
+    ['a. ', 'b. ', 'c. ', 'd. '],
+    ['a. ', 'b. ', 'c. ', 'd. '],
+    ['a. ', 'b. ', 'c. ', 'd. '],
+    ['a. ', 'b. ', 'c. ', 'd. '],
+    ['a. ', 'b. ', 'c. ', 'd. '],
+    ['a. ', 'b. ', 'c. ', 'd. '],
+    ['a. ', 'b. ', 'c. ', 'd. '],
+    ['a. ', 'b. ', 'c. ', 'd. '],
+    ['a. ', 'b. ', 'c. ', 'd. '],
+    ['a. ', 'b. ', 'c. ', 'd. '],
+    ['a. ', 'b. ', 'c. ', 'd. '],
+    ['a. ', 'b. ', 'c. ', 'd. '],
+    ['a. ', 'b. ', 'c. ', 'd. '],
+    ['a. ', 'b. ', 'c. ', 'd. '],
+    ['a. ', 'b. ', 'c. ', 'd. '],
+    ['a. ', 'b. ', 'c. ', 'd. '],
+    ['a. ', 'b. ', 'c. ', 'd. '],
+    ['a. ', 'b. ', 'c. ', 'd. '],
+    ['a. ', 'b. ', 'c. ', 'd. '],
+    ['a. ', 'b. ', 'c. ', 'd. '],
+    ['a. ', 'b. ', 'c. ', 'd. '],
+    ['a. ', 'b. ', 'c. ', 'd. '],
+    ['a. ', 'b. ', 'c. ', 'd. '],
+    ['a. ', 'b. ', 'c. ', 'd. '],
+    ['a. ', 'b. ', 'c. ', 'd. '],
+    ['a. ', 'b. ', 'c. ', 'd. '],
+    ['a. ', 'b. ', 'c. ', 'd. '],
+    ['a. ', 'b. ', 'c. ', 'd. ']
+]
+
+
+const player1 = 'Player 1 has WON!';
+const player2 = 'Player 2 has WON!';
+
+
+const start = document.querySelector('#start')
+const submit = document.querySelector('#submit')
+const reset = document.querySelector('#reset')
+
+
+
+start.addEventListener('click', onclick)
+function clickStart() {
+
+}
+console.log('start button clicked')
+
+
+submit.addEventListener('click',)
+reset.addEventListener('click',)
