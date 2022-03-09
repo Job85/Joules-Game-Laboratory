dfe7432de5d19f066a34e982bfa3ce0e73d5d22f
Job85 <julianjernigan@ymail.com>
2022-03-09 11:15:32 -0500

    updated and styled  container with clue buttons, playerContainer (added players and score)

--- One_Ring/app.js
+++ One_Ring/app.js
@@ -1,155 +1,23 @@
 
-const easyLvl = document.querySelector('#easyLvl')
-const mediumLvl = document.querySelector('#mediumLvl')
-const hardLvl = document.querySelector('#hardLvl')
-const expertLvl = document.querySelector('#expertLvl')
-
-// const shuffleQuestions, currentQuestionIndex
-
-const btnA = document.querySelector('#btnA')
-const btnB = document.querySelector('#btnB')
-const btnC = document.querySelector('#btnC')
-const btnD = document.querySelector('#btnD')
-
-const submit = document.querySelector('#submit')
-const reset = document.querySelector('#reset')
-let player1Score = 0;
-let player2Score = 0;
-// answerArray = [btnA, btnB, btnC, btnD];
-// playerQuestions = [];
-
-
-// eLvlQuestions = [
-//     'How long did J.R.R Tolkien spend completing the Lord of the Rings books?',
-//     'How many rings were given to the Dwarf-kings?',
-//     'What was Frodo\'s Father\'s name?',
-//     'What was Sam\'s spouse\'s name?',
-//     'How many Nazgul were there?',
-//     'Who was the leader of the Nazgul?',
-//     'Who were the Istari?',
-//     'What was the Dark Lord\'s name?',
-//     'How many were the Fathers of the Dwarves?',
-//     'Who were the Wood-elves?',
-//     'What is the name of Elrond\'s daughter?'
-// ];
-// eLvlAnswers = [
-//     ['a. 5 years', 'b. 12 years', 'c. 25 years', 'D. 40 years'],
-//     ['a. 9 rings', 'B. 7 rings', 'c. 3 rings', ' d. 1 ring'],
-//     ['a. Bilbo', 'B. Drogo', 'c. Fosco', 'd. Largo'],
-//     ['A. Rose', 'b. Elanor', 'c. Bree', 'd. Arwen'],
-//     ['a. 6', 'b. 12', 'C. 9', 'd. 15'],
-//     ['a. Grima', 'b. Dunedain', 'c. Khamul', 'D. Witch-king of Angmar']
-//     ['A. Wizards', 'b. Balrog', 'c. Ents', 'd. Dragons'],
-//     ['a. Saruman', 'b. Melkor', 'c. Smaug', 'D. Sauron'],
-//     ['a. 12', 'B. 7', 'c. 6', 'd. 10'],
-//     ['A. Silvan', 'b. Laiquendi', 'c. Vanyar', 'd. Falmari'],
-//     ['a. ', 'b. ', 'c. ', 'd. Arwen'],
-//     ['a. ', 'b. ', 'c. ', 'd. '],
-//     ['a. ', 'b. ', 'c. ', 'd. '],
-//     ['a. ', 'b. ', 'c. ', 'd. '],
-//     ['a. ', 'b. ', 'c. ', 'd. '],
-//     ['a. ', 'b. ', 'c. ', 'd. '],
-//     ['a. ', 'b. ', 'c. ', 'd. '],
-//     ['a. ', 'b. ', 'c. ', 'd. '],
-//     ['a. ', 'b. ', 'c. ', 'd. '],
-//     ['a. ', 'b. ', 'c. ', 'd. '],
-//     ['a. ', 'b. ', 'c. ', 'd. '],
-//     ['a. ', 'b. ', 'c. ', 'd. '],
-//     ['a. ', 'b. ', 'c. ', 'd. '],
-//     ['a. ', 'b. ', 'c. ', 'd. '],
-//     ['a. ', 'b. ', 'c. ', 'd. '],
-//     ['a. ', 'b. ', 'c. ', 'd. '],
-//     ['a. ', 'b. ', 'c. ', 'd. '],
-//     ['a. ', 'b. ', 'c. ', 'd. '],
-//     ['a. ', 'b. ', 'c. ', 'd. '],
-//     ['a. ', 'b. ', 'c. ', 'd. '],
-//     ['a. ', 'b. ', 'c. ', 'd. '],
-//     ['a. ', 'b. ', 'c. ', 'd. '],
-//     ['a. ', 'b. ', 'c. ', 'd. '],
-//     ['a. ', 'b. ', 'c. ', 'd. '],
-//     ['a. ', 'b. ', 'c. ', 'd. '],
-//     ['a. ', 'b. ', 'c. ', 'd. '],
-//     ['a. ', 'b. ', 'c. ', 'd. '],
-//     ['a. ', 'b. ', 'c. ', 'd. '],
-//     ['a. ', 'b. ', 'c. ', 'd. '],
-//     ['a. ', 'b. ', 'c. ', 'd. ']
-// ];
-// correctAnswers = ['d', 'b', 'b', 'a', 'c', 'd', 'a', 'd', 'b', 'a'],
-//     playerAnswers = [];
-
-
-easyLvl.addEventListener('click', chooseEasy);
-mediumLvl.addEventListener('click', chooseMedium)
-hardLvl.addEventListener('click', chooseHard)
-expertLvl.addEventListener('click', chooseExpert)
-
-btnA.addEventListener('click', selectA)
-btnB.addEventListener('click', selectB)
-btnC.addEventListener('click', selectC)
-btnD.addEventListener('click', selectD)
-submit.addEventListener('click', submitQ)
-
-
-function chooseEasy() {
-
+const elements = [
+    { buttonText: 'Button 1' },
+    { buttonText: 'Button 2' },
+    { buttonText: 'Button 3' }
+];
+const parent = document.querySelector('#button-container');
+for (let i = 0; i < clues.length; i++) {
+    let button = document.createElement('button');
+    button.innerHTML = clues[i].buttonText;
+    parent.appendChild(button);
 }
+// function makeGrid(rows, clms) {
+//     container.style.setProperty('--grid-rows', rows);
+//     container.style.setProperty('--grid-cols', clms);
+//     for (c = 0; c < (rows * clms); c++) {
+//         let cell = document.createElement('div');
+//         cell.innerText = (c + 1);
+//         container.appendChild(cell).className = 'grid-item';
+//     };
+// };
 
-function chooseMedium() {
-
-};
-
-function chooseHard() {
-
-};
-
-function chooseExpert() {
-
-};
-
-function pushNextQuestion() {
-
-}
-
-function selectAnswer() {
-
-}
-function selectA() {
-
-}
-
-function selectB() {
-
-}
-
-function selectC() {
-
-}
-function selectD() {
-
-}
-
-function submitQ() {
-
-}
-
-
-
-
-
-
-// reset.addEventListener('click', onclick)
-
-
-
-
-
-
-
-
-
-//correct answer sequence
-
-//incorrect answer sequence
-
-
-
+// makeGrid(5, 6);
