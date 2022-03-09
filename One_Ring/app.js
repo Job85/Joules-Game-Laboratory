e8d4f1d09994c7754cce8df6da725c0fca26dfe2
Job85 <julianjernigan@ymail.com>
2022-03-09 13:30:13 -0500

    added btn array

--- One_Ring/app.js
+++ One_Ring/app.js
@@ -1,23 +1,72 @@
+// const cell1 = document.querySelector('#elves100');
+// const cell2 = document.querySelector('#dwarves100');
+// const cell3 = document.querySelector('#hobbits100');
+// const cell4 = document.querySelector('#books100');
+// const cell5 = document.querySelector('#timelines100');
+// const cell6 = document.querySelector('#potpourri100');
+// const cell7 = document.querySelector('#elves200');
+// const cell8 = document.querySelector('#dwarves200');
+// const cell9 = document.querySelector('#hobbits200');
+// const cell10 = document.querySelector('#books200');
+// const cell11 = document.querySelector('#timelines200');
+// const cell12 = document.querySelector('#potpourri200');
+// const cell13 = document.querySelector('#elves300');
+// const cell14 = document.querySelector('#dwarves300');
+// const cell15 = document.querySelector('#hobbits300');
+// const cell16 = document.querySelector('#books300');
+// const cell17 = document.querySelector('#timelines300');
+// const cell18 = document.querySelector('#potpourri300');
+// const cell19 = document.querySelector('#elves400');
+// const cell20 = document.querySelector('#dwarves400');
+// const cell21 = document.querySelector('#hobbits400');
+// const cell22 = document.querySelector('#books400');
+// const cell23 = document.querySelector('#timelines400');
+// const cell24 = document.querySelector('#potpourri400');
+// const cell25 = document.querySelector('#elves500');
+// const cell26 = document.querySelector('#dwarves500');
+// const cell27 = document.querySelector('#hobbits500');
+// const cell28 = document.querySelector('#books500');
+// const cell29 = document.querySelector('#timelines500');
+// const cell30 = document.querySelector('#potpourri500');
 
-const elements = [
-    { buttonText: 'Button 1' },
-    { buttonText: 'Button 2' },
-    { buttonText: 'Button 3' }
-];
-const parent = document.querySelector('#button-container');
-for (let i = 0; i < clues.length; i++) {
-    let button = document.createElement('button');
-    button.innerHTML = clues[i].buttonText;
-    parent.appendChild(button);
+// const buttons = document.querySelector('.btn');
+
+const btn = [
+    document.querySelector('#elves100'),
+    document.querySelector('#dwarves100'),
+    document.querySelector('#hobbits100'),
+    document.querySelector('#books100'),
+    document.querySelector('#timelines100'),
+    document.querySelector('#potpourri100'),
+    document.querySelector('#elves200'),
+    document.querySelector('#dwarves200'),
+    document.querySelector('#hobbits200'),
+    document.querySelector('#books200'),
+    document.querySelector('#timelines200'),
+    document.querySelector('#potpourri200'),
+    document.querySelector('#elves300'),
+    document.querySelector('#dwarves300'),
+    document.querySelector('#hobbits300'),
+    document.querySelector('#books300'),
+    document.querySelector('#timelines300'),
+    document.querySelector('#potpourri300'),
+    document.querySelector('#elves400'),
+    document.querySelector('#dwarves400'),
+    document.querySelector('#hobbits400'),
+    document.querySelector('#books400'),
+    document.querySelector('#timelines400'),
+    document.querySelector('#potpourri400'),
+    document.querySelector('#elves500'),
+    document.querySelector('#dwarves500'),
+    document.querySelector('#hobbits500'),
+    document.querySelector('#books500'),
+    document.querySelector('#timelines500'),
+    document.querySelector('#potpourri500')
+]
+
+
+btn.addEventListener('click', showClue)
+
+function showClue() {
+    console.log('show clue')
 }
-// function makeGrid(rows, clms) {
-//     container.style.setProperty('--grid-rows', rows);
-//     container.style.setProperty('--grid-cols', clms);
-//     for (c = 0; c < (rows * clms); c++) {
-//         let cell = document.createElement('div');
-//         cell.innerText = (c + 1);
-//         container.appendChild(cell).className = 'grid-item';
-//     };
-// };
-
-// makeGrid(5, 6);
